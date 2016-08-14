07a2ffdb26f824446573057bcbb6a156d45ff346
Sidhant Saini <sidhantsaini01@gmail.com>
2016-08-14 18:25:42 -0400

    refactor middleware

---
+++ middleware.js
@@ -0,0 +1,13 @@
+var middleware = {
+	requiredAuthentication: function(req,res,next){
+		console.log('private route hit');
+		next();
+	},
+	logger: function(req,res,next){
+
+		console.log(new Date().toString()+' '+req.method+' '+req.originalUrl);
+		next();
+	}
+};
+
+module.exports = middleware;
--- server.js
+++ server.js
@@ -1,17 +1,8 @@
 var express = require('express');
 var app = express();
+var middleware = require('./middleware.js')
 
-var middleware = {
-	requiredAuthentication: function(req,res,next){
-		console.log('private route hit');
-		next();
-	},
-	logger: function(req,res,next){
 
-		console.log(new Date().toString()+' '+req.method+' '+req.originalUrl);
-		next();
-	}
-};
 
 app.use(middleware.logger);
 

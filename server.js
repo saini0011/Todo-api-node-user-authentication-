a15a3b41fc7eae8ac9c7862418ef0fd8d5a0baca
Sidhant Saini <sidhantsaini01@gmail.com>
2016-08-20 16:06:31 -0400

    Pick and trim

--- server.js
+++ server.js
@@ -2,13 +2,14 @@ var express = require('express');
 var app = express();
 var middleware = require('./middleware.js');
 var bodyParser=require('body-parser');
+var _ = require('underscore');
 app.use(bodyParser.json());
 var port = process.env.PORT || 3000;
 var todos = [];
 var todoId = 1;
 
 app.get('/',function(req,res){
-	res.json('Todod Api');
+	res.send('Todod Api');
 });
 
 app.get('/todos',function(req,res){
@@ -41,27 +42,60 @@ app.get('/todos/:id',function(req,res){
 
 app.post('/todos',function(req,res){
 
-	var body = req.body;
+	var body =_.pick(req.body,'desc','completed');
+	if(_.isBoolean(body.completed)&&_.isString(body.desc)&&body.desc.trim().length>0){
+		console.log(body);
 	body.id = todoId;
+	body.desc = body.desc.trim();
 	todos.push(body);
 	todoId++;
 	res.send(body);
+
+	}
+    else{
+	    res.status(400).send("Bad Request Data");	
+	  }
 
 
 });
 
 app.put('/todos/:id', function(req,res){
-	var body = req.body;
-	var id = parseInt(req.params.id);
-	todos.ForEach(function(t){
-		if(t.id===id){
-			t.desc = body.desc;
-			t.completed = body.completed;
 
-		}
+	if(todos.length>0){
+		//console.log('url hit');
+  var body = _.pick(req.body,'desc','completed');
+  if(_.isBoolean(body.completed)&&_.isString(body.desc)&&body.desc.trim().length>0){
+  	var matchedTodo;
+  var id = parseInt(req.params.id);
+  var response = todos.forEach(function(t){
+	  if(t.id===id){
+        t.desc = body.desc;
+        t.completed = body.completed;
+        res.send(t);
+        //return;
+	  }
+	  else{
+	    res.status(404).send("Id Not Found");	
+	  }
 	});
+  }
+  	else{
+	    res.status(400).send("Bad Request Data");	
+	  }
+
+  
+ }
+
+ else{
+	res.status(404).send("Bad Request Data");
+}
 
 });
+
+
+
+
+
 
 
 

8a9de677b5f5850cf1b737c3f0d4d186a45cf655
Sidhant Saini <sidhantsaini01@gmail.com>
2016-08-20 13:18:20 -0400

    post added

--- server.js
+++ server.js
@@ -1,17 +1,11 @@
 var express = require('express');
 var app = express();
-var middleware = require('./middleware.js')
+var middleware = require('./middleware.js');
+var bodyParser=require('body-parser');
+app.use(bodyParser.json());
 var port = process.env.PORT || 3000;
-var todos = [{
-	id:1,
-	desc: 'Meet mom for lunch',
-	completed: false
-},
-{
-	id:2,
-	desc: 'Go to market',
-	completed: false
-}];
+var todos = [];
+var todoId = 1;
 
 app.get('/',function(req,res){
 	res.json('Todod Api');
@@ -25,7 +19,7 @@ app.get('/todos',function(req,res){
 
 app.get('/todos/:id',function(req,res){
 		var id = parseInt(req.params.id);
-		console.log(id);
+		//console.log(id);
 		var matchedTodo;
 	todos.forEach(function(f){
 
@@ -42,6 +36,30 @@ app.get('/todos/:id',function(req,res){
 		}
 	
 			
+
+});
+
+app.post('/todos',function(req,res){
+
+	var body = req.body;
+	body.id = todoId;
+	todos.push(body);
+	todoId++;
+	res.send(body);
+
+
+});
+
+app.put('/todos/:id', function(req,res){
+	var body = req.body;
+	var id = parseInt(req.params.id);
+	todos.ForEach(function(t){
+		if(t.id===id){
+			t.desc = body.desc;
+			t.completed = body.completed;
+
+		}
+	});
 
 });
 
